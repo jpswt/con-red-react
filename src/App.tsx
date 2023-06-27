ad58755d8599de915a90b9fa288d9ea27df22eeb
jpswt <joel_swt@yahoo.com>
2023-06-26 22:26:55 -0500

    first commit context-reducer

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Cart from './components/Cart';
+import Header from './components/Header';
+import { useState } from 'react';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
+
+function App() {
+	const [viewCart, setViewCart] = useState<boolean>(false);
+
+	const pageContent = viewCart ? <Cart /> : <ProductList />;
+
+	const content = (
+		<>
+			<Header viewCart={viewCart} setViewCart={setViewCart} />
+			{pageContent}
+			<Footer viewCart={viewCart} />
+		</>
+	);
+	return <>{content}</>;
+}
+
+export default App;
